f2f627bd0a2f410592619dc00fbaba83315b6ab3
Abril Barros <barros.abril.ivon@gmail.com>
2025-09-15 09:12:50 -0300

    ejercicio n3 resuelto y corrección de indentación

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("====== Ejercicio 2 ======")
 cancion_1 = cancion.Cancion("From Eden", 200, "indie folk")
 cancion_2 = cancion.Cancion("The Perfect Girl", 189, "darkwave")
 cancion_3 = cancion.Cancion("Pretty Girl", 180, "bedroom pop")
+
+print("====== Ejercicio 3 ======")
+
+print(cancion_1.obtener_genero())
+print(cancion_2.obtener_genero())
+print(cancion_3.obtener_genero())
