1bdef35a67ce4c4ab53f8990fb3db9ec6d84f51c
Abril Barros <barros.abril.ivon@gmail.com>
2025-09-15 08:51:17 -0300

    ejercicio n2 resuelto

---
+++ main.py
@@ -0,0 +1,6 @@
+import cancion
+
+print("====== Ejercicio 2 ======")
+cancion_1 = cancion.Cancion("From Eden", 200, "indie folk")
+cancion_2 = cancion.Cancion("The Perfect Girl", 189, "darkwave")
+cancion_3 = cancion.Cancion("Pretty Girl", 180, "bedroom pop")
