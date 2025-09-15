e366e262ae2f85697d31c5d751748b428355badc
Abril Barros <barros.abril.ivon@gmail.com>
2025-09-15 08:19:19 -0300

    ejercicio 1 resuelto

---
+++ cancion.py
@@ -0,0 +1,28 @@
+# Ejercicio 1
+
+# Clase que modela una canción con su nombre, duración (en segundos) y género.
+class Cancion:
+    def __init__(self, nombre, duracion, genero):
+        self.nombre = nombre
+        self.duracion = duracion
+        self.genero = genero
+        
+# Comandos (setters)
+def establecer_nombre(self, nombre):
+    self.nombre = nombre
+    
+def establecer_duracion(self, duracion):
+    self.duracion = duracion
+    
+def establecer_genero(self, genero):
+    self.genero = genero
+    
+# Consultas (getters)
+def obtener_nombre(self):
+    return self.nombre
+
+def obtener_duracion(self):
+    return self.duracion
+
+def obtener_genero(self):
+    return self.genero
